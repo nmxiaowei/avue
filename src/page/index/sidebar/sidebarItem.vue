2961070e743f20a23c29d9ed45a3a8ba70ad7153
smallwei <1634566606@qq.com>
2018-03-31 09:34:03 +0800

    fixed #IILED

--- src/page/index/sidebar/sidebarItem.vue
+++ src/page/index/sidebar/sidebarItem.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="menu-wrapper">
     <template v-for="(item,index) in menu">
-      <el-menu-item v-if="item.children.length===0 " :index="item.href" @click="open(item)" :key="item.label">
+      <el-menu-item v-if="item.children.length===0 " :index="filterPath(item.href,index)" @click="open(item)" :key="item.label">
         <i :class="item.icon"></i>
         <span slot="title">{{item.label}}</span>
       </el-menu-item>
-      <el-submenu v-else :index="item.label" :key="item.name">
+      <el-submenu v-else :index="filterPath(item.name,index)" :key="item.name">
         <template slot="title">
           <i :class="item.icon"></i>
           <span slot="title" :class="{display:!show}">{{item.label}}</span>
         </template>
         <template v-for="(child,cindex) in item.children">
-          <el-menu-item :index="child.href" @click="open(child)" v-if="child.children.length==0" :key="cindex">
+          <el-menu-item :index="filterPath(child.href,cindex)" @click="open(child)" v-if="child.children.length==0" :key="cindex">
             <i :class="child.icon"></i>
             <span slot="title">{{child.label}}</span>
           </el-menu-item>
@@ -39,6 +39,9 @@ export default {
   created() {},
   mounted() {},
   methods: {
+    filterPath(path, index) {
+      return path == null ? index + "" : path;
+    },
     open(item) {
       this.$router.push({
         path: resolveUrlPath(item.href, item.label),
@@ -49,7 +52,8 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.display {
+.display,
+.display + .el-submenu__icon-arrow {
   display: none;
 }
 </style>
