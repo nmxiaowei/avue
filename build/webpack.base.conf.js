7d0fe6f71664cf8d801d9704bce23d1f8b525540
zclhl <459417805@qq.com>
2018-05-30 18:42:06 +0800

    !6 兼容ie Merge pull request !6 from zclhl/master

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -60,7 +60,13 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
-				include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+				include: [
+					resolve('src'), 
+					resolve('test'), 
+					resolve('packages'),
+					resolve('node_modules/element-ui/src'),
+					resolve('node_modules/webpack-dev-server/client')
+				]
 			},
 			{
 				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
