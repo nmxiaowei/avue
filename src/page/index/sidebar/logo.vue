74eed6b91bda5b963cda6322793cad25adf69d9c
zennu <zennuchan@163.com>
2018-06-04 23:25:49 +0800

    左侧菜单底部出线滚动条

--- src/page/index/sidebar/logo.vue
+++ src/page/index/sidebar/logo.vue
@@ -55,7 +55,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 230px;
+  // width: 230px;
   height: 64px;
   line-height: 64px;
   background: #002140;
