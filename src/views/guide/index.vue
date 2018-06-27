c21e78c7a0a4ae30855d3c2cf39b8fa05b34c793
smallwei <1634566606@qq.com>
2018-06-27 17:48:27 +0800

    修复引导页

--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -1,69 +1,66 @@
 <template>
   <div class="guide-container">
-    <filters :option="option" @get-query="getQuery"></filters>
-    <el-button icon='el-icon-question' type="primary" @click="guide">运行引导页</el-button>
+    <el-button icon='el-icon-question'
+               type="primary"
+               @click="guide">运行引导页</el-button>
   </div>
 </template>
 
 <script>
-import filters from './filter/'
-import * as Driver from 'driver.js' // import driver.js
-import 'driver.js/dist/driver.min.css' // import driver.js css
-import steps from './defineSteps'
+import * as Driver from "driver.js"; // import driver.js
+import "driver.js/dist/driver.min.css"; // import driver.js css
+import steps from "./defineSteps";
 
 export default {
-  name: 'guide',
-  components: {
-    filters
-  },
+  name: "guide",
   data() {
     return {
       option: {
         cloumns: [
           {
-            name: '区域',
-            prop: 'area',
-            type: 'select',
+            name: "区域",
+            prop: "area",
+            type: "select",
             children: [
               {
-                label: '婚房1',
+                label: "婚房1",
                 value: 1
               },
               {
-                label: '婚房2',
+                label: "婚房2",
                 value: 2
               },
               {
-                label: '婚房3',
+                label: "婚房3",
                 value: 3
               },
               {
-                label: '婚房4',
+                label: "婚房4",
                 value: 4
               }
             ]
           },
           {
-            name: '价格',
-            prop: 'price',
-            type: 'cascader',
+            name: "价格",
+            prop: "price",
+            type: "cascader",
             children: [
               {
-                label: '婚房1',
+                label: "婚房1",
                 value: 1,
                 children: [
                   {
-                    label: '婚房1',
+                    label: "婚房1",
                     value: 2
                   }
                 ]
               },
               {
-                label: '婚房2',
+                label: "婚房2",
                 value: 2,
                 children: [
                   {
-                    label: '婚房2',
+                    label: "婚房2",
                     value: 2
                   }
                 ]
@@ -71,69 +68,69 @@ export default {
             ]
           },
           {
-            name: '排序',
-            prop: 'type',
-            type: 'select',
+            name: "排序",
+            prop: "type",
+            type: "select",
             children: [
               {
-                label: '从高到低',
+                label: "从高到低",
                 value: 1
               },
               {
-                label: '从上到下',
+                label: "从上到下",
                 value: 2
               },
               {
-                label: '从高到低',
+                label: "从高到低",
                 value: 3
               }
             ]
           },
           {
-            name: '更多',
-            prop: 'more',
-            type: 'checkbox',
+            name: "更多",
+            prop: "more",
+            type: "checkbox",
             children: [
               {
-                label: '特色',
-                prop: 'tese',
+                label: "特色",
+                prop: "tese",
                 dic: [
                   {
-                    label: '婚房',
+                    label: "婚房",
                     value: 0
                   },
                   {
-                    label: '毛皮房',
+                    label: "毛皮房",
                     value: 1
                   },
                   {
-                    label: '婚房',
+                    label: "婚房",
                     value: 2
                   },
                   {
-                    label: '毛皮房',
+                    label: "毛皮房",
                     value: 3
                   },
                   {
-                    label: '婚房',
+                    label: "婚房",
                     value: 4
                   },
                   {
-                    label: '毛皮房',
+                    label: "毛皮房",
                     value: 5
                   }
                 ]
               },
               {
-                label: '面积',
-                prop: 'mianji',
+                label: "面积",
+                prop: "mianji",
                 dic: [
                   {
-                    label: '60以下',
+                    label: "60以下",
                     value: 0
                   },
                   {
-                    label: '60-80',
+                    label: "60-80",
                     value: 1
                   }
                 ]
@@ -143,19 +140,19 @@ export default {
         ]
       },
       driver: null
-    }
+    };
   },
   mounted() {
-    this.driver = new Driver()
+    this.driver = new Driver();
   },
   methods: {
     guide() {
-      this.driver.defineSteps(steps)
-      this.driver.start()
+      this.driver.defineSteps(steps);
+      this.driver.start();
     },
     getQuery(params) {
-      console.log(prarms)
+      console.log(prarms);
     }
   }
-}
+};
 </script>
