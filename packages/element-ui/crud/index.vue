65c3bdfd2a5ae493e1f09c53165f8c244fb99a6f
smallwei <1634566606@qq.com>
2019-02-27 17:04:38 +0800

    feat crud slot header

--- packages/element-ui/crud/index.vue
+++ packages/element-ui/crud/index.vue
@@ -71,7 +71,7 @@
           <slot name="empty"
                 v-if="$slots.empty"></slot>
           <avue-empty v-else
-                      :size="50"
+                      size="50"
                       image="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNDEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoMCAxKSIgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj4KICAgIDxlbGxpcHNlIGZpbGw9IiNGNUY1RjUiIGN4PSIzMiIgY3k9IjMzIiByeD0iMzIiIHJ5PSI3Ii8+CiAgICA8ZyBmaWxsLXJ1bGU9Im5vbnplcm8iIHN0cm9rZT0iI0Q5RDlEOSI+CiAgICAgIDxwYXRoIGQ9Ik01NSAxMi43Nkw0NC44NTQgMS4yNThDNDQuMzY3LjQ3NCA0My42NTYgMCA0Mi45MDcgMEgyMS4wOTNjLS43NDkgMC0xLjQ2LjQ3NC0xLjk0NyAxLjI1N0w5IDEyLjc2MVYyMmg0NnYtOS4yNHoiLz4KICAgICAgPHBhdGggZD0iTTQxLjYxMyAxNS45MzFjMC0xLjYwNS45OTQtMi45MyAyLjIyNy0yLjkzMUg1NXYxOC4xMzdDNTUgMzMuMjYgNTMuNjggMzUgNTIuMDUgMzVoLTQwLjFDMTAuMzIgMzUgOSAzMy4yNTkgOSAzMS4xMzdWMTNoMTEuMTZjMS4yMzMgMCAyLjIyNyAxLjMyMyAyLjIyNyAyLjkyOHYuMDIyYzAgMS42MDUgMS4wMDUgMi45MDEgMi4yMzcgMi45MDFoMTQuNzUyYzEuMjMyIDAgMi4yMzctMS4zMDggMi4yMzctMi45MTN2LS4wMDd6IiBmaWxsPSIjRkFGQUZBIi8+CiAgICA8L2c+CiAgPC9nPgo8L3N2Zz4K"
                       :desc="tableOption.emptyText || 暂无数据"></avue-empty>
         </div>
@@ -106,7 +106,7 @@
 
       <el-table-column v-for="(column,index) in columnOption"
                        :prop="column.prop"
-                       :key="index"
+                       :key="column.prop"
                        filter-placement="bottom-end"
                        :filters="handleFilters(column)"
                        :filter-method="column.filter? handleFiltersMethod : undefined"
@@ -114,6 +114,7 @@
                        :show-overflow-tooltip="column.overHidden"
                        :min-width="column.minWidth"
                        :sortable="column.sortable"
+                       :render-header="column.renderHeader"
                        :align="column.align || tableOption.align"
                        :header-align="column.headerAlign || tableOption.headerAlign"
                        :width="column.width"
@@ -617,6 +618,15 @@ export default create({
     selectClear() {
       this.$refs.table.clearSelection();
     },
+    toggleRowSelection(row, selected) {
+      this.$refs.table.toggleRowSelection(row, selected);
+    },
+    toggleRowExpansion(row, expanded) {
+      this.$refs.table.toggleRowExpansion(row, expanded);
+    },
+    setCurrentRow(row) {
+      this.$refs.table.setCurrentRow(row);
+    },
     indexMethod(index) {
       return (
         index +
@@ -680,10 +690,6 @@ export default create({
         return list;
       }
       return column.dicFilters;
-    },
-    //设置多选选中
-    setCurrentRow(row) {
-      this.$refs.table.setCurrentRow(row);
     },
     // 选中实例
     toggleSelection(rows) {
