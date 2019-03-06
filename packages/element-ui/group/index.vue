cc0197bf55e3a3dda2a0ad24a8801b9022c0261a
smallwei <1634566606@qq.com>
2019-03-06 08:43:38 +0800

    fix form group header

--- packages/element-ui/group/index.vue
+++ packages/element-ui/group/index.vue
@@ -3,15 +3,17 @@
        v-if="display">
     <el-row span="24"
             :class="b('item')">
-      <div :class="b('header')">
-        <slot name="header"
-              v-if="$slots.header"></slot>
-        <template v-else>
-          <i :class="[icon,b('icon')]"
-             v-if="icon"></i>
-          <h1 :class="b('title')"
-              v-if="label">{{label}}</h1>
-        </template>
+      <div :class="b('header')"
+           v-if="$slots.header">
+        <slot name="header"></slot>
+      </div>
+      <div :class="b('header')"
+           v-else-if="label || icon">
+        <i :class="[icon,b('icon')]"
+           v-if="icon"></i>
+        <h1 :class="b('title')"
+            v-if="label">{{label}}</h1>
+
       </div>
 
       <slot></slot>
