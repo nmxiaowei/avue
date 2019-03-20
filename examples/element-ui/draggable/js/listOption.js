2dabb34d5b939991cf6624f51e3f1a55e936bc10
smallwei <1634566606@qq.com>
2019-03-20 13:55:16 +0800

    test v1.0.9

---
+++ examples/element-ui/draggable/js/listOption.js
@@ -0,0 +1,599 @@
+var list = [{
+  "name": "柱状图",
+  "title": "柱状图",
+  "top": 276,
+  "left": 536,
+  "zIndex": 1,
+  "component": {
+    "width": 1200,
+    "height": 600,
+    "name": "avue-echart-bar",
+    "prop": "bar",
+    "option": {
+      "refreshTime": 5000,
+      "title": "2019年度潍坊城市销售额排名",
+      "titlePostion": "",
+      "titleFontSize": 24,
+      "titleColor": "rgb(115, 170, 229)",
+      "titleLink": "http://avue.top",
+      "info": "这是图表的简介",
+      "width": 1282,
+      "height": 600,
+      "gridX": 105,
+      "gridY": 50,
+      "gridX2": 80,
+      "gridY2": 100,
+      "nameColor": "#eee",
+      "lineColor": "#eee",
+      "xNameFontSize": 16,
+      "yNameFontSize": 16,
+      "labelShow": false,
+      "labelShowFontSize": 14,
+      "labelShowFontWeight": 300,
+      "yAxisInverse": false,
+      "xAxisInverse": false,
+      "xAxisShow": true,
+      "yAxisShow": true,
+      "xAxisSplitLineShow": false,
+      "yAxisSplitLineShow": false,
+      "refresh": 3000,
+      "barWidth": 30,
+      "barRadius": 8,
+      "barColor": [{
+        "color1": "#83bff6",
+        "color2": "#188df0",
+        "postion": 90,
+        "$index": 0,
+        "_show": true
+      }, {
+        "color1": "#23B7E5",
+        "color2": "#564AA3",
+        "postion": 50,
+        "$index": 1,
+        "_show": true
+      }],
+      "barMinHeight": 2
+    },
+    "data": {
+      "categories": ["北京", "上海", "杭州", "深圳", "杭州", "深圳", "广州", "连云港"],
+      "series": [{
+        "name": "手机品牌",
+        "data": [1131776, 1349011, 905718, 1349011, 905718, 656020, 571234, 467397]
+      }, {
+        "name": "其他手机品牌",
+        "data": [1131776, 1349011, 905718, 1349011, 905718, 656020, 571234, 467397]
+      }]
+    }
+  },
+  "index": 0
+}, {
+  "title": "图片框",
+  "name": "图片框",
+  "top": 0,
+  "left": 115,
+  "zIndex": 13,
+  "component": {
+    "width": 1700,
+    "height": 146,
+    "name": "img",
+    "prop": "img",
+    "value": "https://sugar.bce.baidu.com/static/img-templet/pic-2.png"
+  },
+  "index": 1
+}, {
+  "title": "图片框",
+  "name": "图片框",
+  "top": 900,
+  "left": 115,
+  "zIndex": 12,
+  "component": {
+    "width": 1700,
+    "height": 146,
+    "name": "img",
+    "prop": "img",
+    "value": "https://sugar.bce.baidu.com/static/img-templet/pic-1.png"
+  },
+  "index": 2
+}, {
+  "title": "图片框",
+  "name": "图片框",
+  "top": 20,
+  "left": 20,
+  "zIndex": 11,
+  "component": {
+    "name": "img",
+    "prop": "img",
+    "value": "https://sugar.bce.baidu.com/static/img-templet/pic-8.png "
+  },
+  "index": 3
+}, {
+  "title": "图片框",
+  "name": "图片框",
+  "top": 344,
+  "left": 10,
+  "zIndex": 10,
+  "component": {
+    "width": 80,
+    "height": 80,
+    "name": "img",
+    "prop": "img",
+    "value": "https://sugar.bce.baidu.com/static/img-templet/pic-4.png"
+  },
+  "index": 4
+}, {
+  "title": "图片框",
+  "name": "图片框",
+  "top": 340,
+  "left": 230,
+  "zIndex": 9,
+  "component": {
+    "width": 80,
+    "height": 80,
+    "name": "img",
+    "prop": "img",
+    "value": "https://sugar.bce.baidu.com/static/img-templet/pic-5.png"
+  },
+  "index": 5
+}, {
+  "title": "图片框",
+  "name": "图片框",
+  "top": 479,
+  "left": 10,
+  "zIndex": 8,
+  "component": {
+    "width": 80,
+    "height": 80,
+    "name": "img",
+    "prop": "img",
+    "value": "https://sugar.bce.baidu.com/static/img-templet/pic-6.png"
+  },
+  "index": 6
+}, {
+  "title": "图片框",
+  "name": "图片框",
+  "top": 481,
+  "left": 232,
+  "zIndex": 7,
+  "component": {
+    "width": 80,
+    "height": 80,
+    "name": "img",
+    "prop": "img",
+    "value": "https://sugar.bce.baidu.com/static/img-templet/pic-7.png"
+  },
+  "index": 7
+}, {
+  "title": "翻牌器",
+  "name": "翻牌器",
+  "top": 230,
+  "left": 1140,
+  "zIndex": 6,
+  "component": {
+    "width": 750,
+    "height": 120,
+    "name": "avue-echart-flop",
+    "prop": "flop",
+    "data": "1201009,2",
+    "list": ["https://sugar.bce.baidu.com/static/img-templet/border-1.png", "https://sugar.bce.baidu.com/static/img-templet/border-2.png"],
+    "option": {
+      "type": "img",
+      "borderColor": "#fff",
+      "borderWidth": 3,
+      "backgroundImage": "https://sugar.bce.baidu.com/static/img-templet/border-1.png",
+      "fontSize": 64,
+      "fontWeight": "normal",
+      "split": 3,
+      "backgroundColor": "rgb(15, 57, 107)",
+      "color": "#fff"
+    }
+  },
+  "index": 8
+}, {
+  "title": "翻牌器",
+  "name": "翻牌器",
+  "top": 344,
+  "left": 100,
+  "zIndex": 5,
+  "component": {
+    "width": 230,
+    "height": 100,
+    "name": "avue-echart-flop",
+    "prop": "flop",
+    "data": "71.9",
+    "list": ["https://sugar.bce.baidu.com/static/img-templet/border-1.png", "https://sugar.bce.baidu.com/static/img-templet/border-2.png"],
+    "option": {
+      "type": "",
+      "suffixText": "订单总数(万)",
+      "suffixTextAlign": "",
+      "suffixSplit": 5,
+      "suffixColor": "rgba(176, 194, 249, 1)",
+      "suffixFontSize": 16,
+      "borderColor": "#fff",
+      "borderWidth": 3,
+      "backgroundImage": "https://sugar.bce.baidu.com/static/img-templet/border-1.png",
+      "fontSize": 42,
+      "fontWeight": "normal",
+      "split": 0,
+      "backgroundColor": "",
+      "color": "#fff"
+    }
+  },
+  "index": 9
+}, {
+  "title": "翻牌器",
+  "name": "翻牌器",
+  "top": 346,
+  "left": 320,
+  "zIndex": 4,
+  "component": {
+    "width": 230,
+    "height": 100,
+    "name": "avue-echart-flop",
+    "prop": "flop",
+    "data": "16.24",
+    "list": ["https://sugar.bce.baidu.com/static/img-templet/border-1.png", "https://sugar.bce.baidu.com/static/img-templet/border-2.png"],
+    "option": {
+      "type": "",
+      "suffixText": "销售总量（万元）",
+      "suffixTextAlign": "",
+      "suffixSplit": 5,
+      "suffixColor": "rgba(176, 194, 249, 1)",
+      "suffixFontSize": 16,
+      "borderColor": "#fff",
+      "borderWidth": 3,
+      "backgroundImage": "https://sugar.bce.baidu.com/static/img-templet/border-1.png",
+      "fontSize": 42,
+      "fontWeight": "normal",
+      "split": 0,
+      "backgroundColor": "",
+      "color": "#fff"
+    }
+  },
+  "index": 10
+}, {
+  "title": "翻牌器",
+  "name": "翻牌器",
+  "top": 478,
+  "left": 102,
+  "zIndex": 3,
+  "component": {
+    "width": 230,
+    "height": 100,
+    "name": "avue-echart-flop",
+    "prop": "flop",
+    "data": "10.5",
+    "list": ["https://sugar.bce.baidu.com/static/img-templet/border-1.png", "https://sugar.bce.baidu.com/static/img-templet/border-2.png"],
+    "option": {
+      "type": "",
+      "suffixText": "人均消费",
+      "suffixTextAlign": "",
+      "suffixSplit": 5,
+      "suffixColor": "rgba(176, 194, 249, 1)",
+      "suffixFontSize": 16,
+      "borderColor": "#fff",
+      "borderWidth": 3,
+      "backgroundImage": "https://sugar.bce.baidu.com/static/img-templet/border-1.png",
+      "fontSize": 42,
+      "fontWeight": "normal",
+      "split": 0,
+      "backgroundColor": "",
+      "color": "#fff"
+    }
+  },
+  "index": 11
+}, {
+  "title": "翻牌器",
+  "name": "翻牌器",
+  "top": 478,
+  "left": 322,
+  "zIndex": 2,
+  "component": {
+    "width": 230,
+    "height": 100,
+    "name": "avue-echart-flop",
+    "prop": "flop",
+    "data": "75",
+    "list": ["https://sugar.bce.baidu.com/static/img-templet/border-1.png", "https://sugar.bce.baidu.com/static/img-templet/border-2.png"],
+    "option": {
+      "type": "",
+      "suffixText": "近10分钟新增订单数(万)",
+      "suffixTextAlign": "",
+      "suffixSplit": 5,
+      "suffixColor": "rgba(176, 194, 249, 1)",
+      "suffixFontSize": 16,
+      "borderColor": "#fff",
+      "borderWidth": 3,
+      "backgroundImage": "https://sugar.bce.baidu.com/static/img-templet/border-1.png",
+      "fontSize": 42,
+      "fontWeight": "normal",
+      "split": 0,
+      "backgroundColor": "",
+      "color": "#fff"
+    }
+  },
+  "index": 12
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 14,
+  "left": 702,
+  "zIndex": 26,
+  "component": {
+    "width": 490,
+    "height": 50,
+    "value": "avuex数据大屏实时展示",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 40,
+      "fontWeight": "bold",
+      "color": "#fff"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 13
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 120,
+  "left": 1122,
+  "zIndex": 25,
+  "component": {
+    "width": 100,
+    "height": 50,
+    "value": "电商数据",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 22,
+      "fontWeight": "normal",
+      "color": "rgba(154, 168, 212, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 14
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 120,
+  "left": 1250,
+  "zIndex": 24,
+  "component": {
+    "width": 100,
+    "height": 50,
+    "value": "Sell Data",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 20,
+      "fontWeight": "normal",
+      "color": "rgba(64, 77, 105, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 15
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 276,
+  "left": 15,
+  "zIndex": 23,
+  "component": {
+    "width": 100,
+    "height": 50,
+    "value": "数据统计",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 22,
+      "fontWeight": "normal",
+      "color": "rgba(154, 168, 212, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 16
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 278,
+  "left": 350,
+  "zIndex": 22,
+  "component": {
+    "width": 150,
+    "height": 50,
+    "value": "Statistics Data",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 20,
+      "fontWeight": "normal",
+      "color": "rgba(64, 77, 105, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 17
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 124,
+  "left": 320,
+  "zIndex": 21,
+  "component": {
+    "width": 150,
+    "height": 50,
+    "value": "完成的百分比",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 20,
+      "fontWeight": "normal",
+      "color": "rgba(64, 77, 105, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 18
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 180,
+  "left": 1104,
+  "zIndex": 20,
+  "component": {
+    "width": 100,
+    "height": 50,
+    "value": "销售量",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 16,
+      "fontWeight": "normal",
+      "color": "rgba(154, 168, 212, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 19
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 180,
+  "left": 1760,
+  "zIndex": 19,
+  "component": {
+    "width": 100,
+    "height": 50,
+    "value": "万元",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 16,
+      "fontWeight": "normal",
+      "color": "rgba(154, 168, 212, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 20
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 646,
+  "left": 15,
+  "zIndex": 18,
+  "component": {
+    "width": 100,
+    "height": 50,
+    "value": "交易统计",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 22,
+      "fontWeight": "normal",
+      "color": "rgba(154, 168, 212, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 21
+}, {
+  "title": "文本框",
+  "name": "文本框",
+  "top": 650,
+  "left": 350,
+  "zIndex": 17,
+  "component": {
+    "width": 150,
+    "height": 50,
+    "value": "Count Data",
+    "style": {
+      "textAlign": "center",
+      "fontSize": 20,
+      "fontWeight": "normal",
+      "color": "rgba(64, 77, 105, 1)"
+    },
+    "name": "text",
+    "prop": "text"
+  },
+  "index": 22
+}, {
+  "title": "进度条",
+  "name": "进度条",
+  "top": 736,
+  "left": 40,
+  "zIndex": 16,
+  "component": {
+    "width": 200,
+    "height": 200,
+    "data": {
+      "label": "订单交易",
+      "value": 300,
+      "data": 30
+    },
+    "option": {
+      "width": 200,
+      "type": "circle",
+      "color": "rgba(154, 168, 212, 1)",
+      "fontSize": 24,
+      "strokeWidth": 14,
+      "fontWeight": "normal",
+      "borderColor": "#23b7e5",
+      "height": 200
+    },
+    "name": "avue-echart-progress",
+    "prop": "progress"
+  },
+  "index": 23
+}, {
+  "title": "进度条",
+  "name": "进度条",
+  "top": 738,
+  "left": 300,
+  "zIndex": 15,
+  "component": {
+    "width": 200,
+    "height": 200,
+    "data": {
+      "label": "人数增涨",
+      "value": 40,
+      "data": 80
+    },
+    "option": {
+      "width": 200,
+      "type": "circle",
+      "color": "rgba(154, 168, 212, 1)",
+      "fontSize": 24,
+      "strokeWidth": 14,
+      "fontWeight": "normal",
+      "borderColor": "#564AA3",
+      "height": 200
+    },
+    "name": "avue-echart-progress",
+    "prop": "progress"
+  },
+  "index": 24
+}, {
+  "title": "进度条",
+  "name": "进度条",
+  "top": 150,
+  "left": 60,
+  "zIndex": 14,
+  "component": {
+    "width": 400,
+    "height": 80,
+    "data": {
+      "label": "人数增涨",
+      "value": 40,
+      "data": 80
+    },
+    "option": {
+      "type": "line",
+      "color": "rgba(154, 168, 212, 1)",
+      "fontSize": 24,
+      "strokeWidth": 18,
+      "fontWeight": "normal",
+      "borderColor": "#564AA3",
+      "width": 400,
+      "height": 80
+    },
+    "name": "avue-echart-progress",
+    "prop": "progress"
+  },
+  "index": 25
+}]
