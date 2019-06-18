907befc2d00e1c617b1ee16bc680cc159b001179
smallwei <1634566606@qq.com>
2019-06-18 15:53:52 +0800

    fix imgBorder block backgroundImage

--- packages/echart/imgBorder/index.vue
+++ packages/echart/imgBorder/index.vue
@@ -10,20 +10,26 @@ import create from "core/echart/create";
 export default create({
   name: "imgborder",
   computed: {
-    styleImgName() {
+    styleImgName () {
       return Object.assign({
         width: "100%",
         height: "100%",
-        borderImageSource: "url(" + this.dataChart + ")",
-        borderImageSlice: "10 16 15 10 fill",
-        borderWidth: "10px 16px 15px 10px",
-        borderStyle: "solid",
-        boxSizing: "border-box",
         backgroundColor:
           this.option.backgroundColor || "rgba(180, 181, 198, 0.1)",
         backgroundClip: "padding-box",
         filter: "blur(0px)"
-      });
+      }, (() => {
+        if (!this.validatenull(this.dataChart)) {
+          return {
+            borderImageSource: "url(" + this.dataChart + ")",
+            borderImageSlice: "10 16 15 10 fill",
+            borderWidth: "10px 16px 15px 10px",
+            borderStyle: "solid",
+            boxSizing: "border-box",
+          }
+        }
+        return {}
+      })())
     }
   },
   methods: {}
