131eaec59268bd0c64590f9dd8fe97df0cbf4406
smallwei <1634566606@qq.com>
2019-08-08 19:17:21 +0800

    fix #235 update crud sortable demo

---
+++ packages/element-ui/time/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-time-picker v-model="text"
+                  :is-range="isRange"
+                  :size="size"
+                  range-separator="至"
+                  :start-placeholder="startPlaceholder"
+                  :end-placeholder="endPlaceholder"
+                  :format="format"
+                  :readonly="readonly"
+                  :clearable="disabled?false:clearable"
+                  :value-format="valueFormat"
+                  :placeholder="placeholder"
+                  @change="handleChange"
+                  @click.native="handleClick"
+                  :disabled="disabled"></el-time-picker>
+</template>
+
+<script>
+import create from "core/create";
+import props from "../../core/common/props.js";
+import event from "../../core/common/event.js";
+export default create({
+  name: "time",
+  mixins: [props(), event()],
+  data () {
+    return {};
+  },
+  props: {
+    startPlaceholder: {
+      type: String,
+      default: "开始时间"
+    },
+    endPlaceholder: {
+      type: String,
+      default: "结束时间"
+    },
+    value: {
+      required: true
+    },
+    valueFormat: {
+      default: ""
+    },
+    type: {
+      default: ""
+    },
+    format: {
+      default: ""
+    }
+  },
+  watch: {},
+  created () { },
+  mounted () { },
+  computed: {
+    isRange () {
+      return this.type === "timerange";
+    }
+  },
+  methods: {}
+});
+</script>
