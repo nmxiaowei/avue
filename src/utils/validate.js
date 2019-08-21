476cf843567430ea2e56b16e7f9e0c5489cdf697
smallwei <1634566606@qq.com>
2019-08-21 22:05:23 +0800

    fix validatenull JSON.stringify quest

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,6 +3,7 @@ import Schema from 'async-validator';
  * 判断是否为空
  */
 export function validatenull(val) {
+  const list = ['$parent', 'children'];
   if (typeof val === 'boolean') {
     return false;
   }
@@ -12,7 +13,9 @@ export function validatenull(val) {
   if (val instanceof Array) {
     if (val.length === 0) return true;
   } else if (val instanceof Object) {
-    delete val.$parent;
+    list.forEach(ele => {
+      delete val[ele];
+    });
     if (JSON.stringify(val) === '{}') return true;
   } else {
     if (
