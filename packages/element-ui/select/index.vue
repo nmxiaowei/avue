2429b41bd9362d21c8cd29251147b27bb8217ce2
smallwei <1634566606@qq.com>
2019-08-22 20:43:17 +0800

    feat select drag

--- packages/element-ui/select/index.vue
+++ packages/element-ui/select/index.vue
@@ -1,56 +1,57 @@
 <template>
-  <div>
-    <el-select v-model="text"
-               :size="size"
-               :multiple="multiple"
-               :filterable="remote?true:filterable"
-               :remote="remote"
-               :readonly="readonly"
-               :remote-method="handleRemoteMethod"
-               :collapse-tags="tags"
-               :clearable="disabled?false:clearable"
-               :placeholder="placeholder"
-               @focus="handleFocus"
-               @blur="handleBlur"
-               @click.native="handleClick"
-               :multiple-limit="limit"
-               :allow-create="allowCreate"
-               :default-first-option="defaultFirstOption"
-               :disabled="disabled">
-      <template v-if="group">
-        <el-option-group v-for="(item,index) in netDic"
-                         :key="index"
-                         :label="getLabelText(item)">
-          <el-option v-for="(citem,cindex) in item[groupsKey]"
-                     :key="cindex"
-                     :disabled="citem[disabledKey]"
-                     :label="getLabelText(citem)"
-                     :value="citem[valueKey]">
-            <slot :name="prop+'Type'"
-                  :label="labelKey"
-                  :value="valueKey"
-                  :item="citem"></slot>
-          </el-option>
-        </el-option-group>
-      </template>
-      <template v-else>
-        <el-option v-for="(item,index) in netDic"
-                   :key="index"
-                   :disabled="item[disabledKey]"
-                   :label="getLabelText(item)"
-                   :value="item[valueKey]">
+  <el-select ref="main"
+             :class="b()"
+             v-model="text"
+             :size="size"
+             :multiple="multiple"
+             :filterable="remote?true:filterable"
+             :remote="remote"
+             :readonly="readonly"
+             :remote-method="handleRemoteMethod"
+             :collapse-tags="tags"
+             :clearable="disabled?false:clearable"
+             :placeholder="placeholder"
+             @focus="handleFocus"
+             @blur="handleBlur"
+             @click.native="handleClick"
+             :multiple-limit="limit"
+             :allow-create="allowCreate"
+             :default-first-option="defaultFirstOption"
+             :disabled="disabled">
+    <template v-if="group">
+      <el-option-group v-for="(item,index) in netDic"
+                       :key="index"
+                       :label="getLabelText(item)">
+        <el-option v-for="(citem,cindex) in item[groupsKey]"
+                   :key="cindex"
+                   :disabled="citem[disabledKey]"
+                   :label="getLabelText(citem)"
+                   :value="citem[valueKey]">
           <slot :name="prop+'Type'"
                 :label="labelKey"
                 :value="valueKey"
-                :item="item"></slot>
+                :item="citem"></slot>
         </el-option>
-      </template>
+      </el-option-group>
+    </template>
+    <template v-else>
+      <el-option v-for="(item,index) in netDic"
+                 :key="index"
+                 :disabled="item[disabledKey]"
+                 :label="getLabelText(item)"
+                 :value="item[valueKey]">
+        <slot :name="prop+'Type'"
+              :label="labelKey"
+              :value="valueKey"
+              :item="item"></slot>
+      </el-option>
+    </template>
 
-    </el-select>
-  </div>
+  </el-select>
 </template>
 
 <script>
+import packages from 'core/packages';
 import create from "core/create";
 import props from "../../core/common/props.js";
 import event from "../../core/common/event.js";
@@ -65,6 +66,10 @@ export default create({
   },
   props: {
     value: {},
+    drag: {
+      type: Boolean,
+      default: false
+    },
     remote: {
       type: Boolean,
       default: false
@@ -105,8 +110,31 @@ export default create({
     }
   },
   created () { },
-  mounted () { },
+  mounted () {
+    if (this.drag) {
+      this.setSort()
+    }
+  },
   methods: {
+    setSort () {
+      if (!window.Sortable) {
+        packages.logs('Sortable');
+        return
+      }
+      const el = this.$refs.main.$el.querySelectorAll('.el-select__tags > span')[0]
+      this.sortable = window.Sortable.create(el, {
+        ghostClass: 'sortable-ghost', // Class name for the drop placeholder,
+        setData: function (dataTransfer) {
+          dataTransfer.setData('Text', '')
+          // to avoid Firefox bug
+          // Detail see : https://github.com/RubaXa/Sortable/issues/1012
+        },
+        onEnd: evt => {
+          const targetRow = this.value.splice(evt.oldIndex, 1)[0]
+          this.value.splice(evt.newIndex, 0, targetRow)
+        }
+      })
+    },
     handleRemoteMethod (query) {
       sendDic({
         url: this.dicUrl.replace("{{key}}", query),
