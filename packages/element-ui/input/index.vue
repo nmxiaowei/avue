1cde65bb590dbb2814cfed359b379a87d9dda983
smallwei <1634566606@qq.com>
2019-12-24 15:39:50 +0800

    fix gitee_I16QT4 tree keylist
    fix input-tree keylist
    
    fix input-tree key list

--- packages/element-ui/input/index.vue
+++ packages/element-ui/input/index.vue
@@ -286,6 +286,9 @@ export default create({
       return list;
     },
     keysList () {
+      if (this.validatenull(this.text)) {
+        return [];
+      }
       return this.multiple ? this.text : [this.text];
     },
     isTree () {
