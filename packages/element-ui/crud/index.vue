6eafdfaf9f310e99da90f67bbde008c475440f4f
smallwei <1634566606@qq.com>
2020-01-19 15:13:42 +0800

    feat crud page.sync

--- packages/element-ui/crud/index.vue
+++ packages/element-ui/crud/index.vue
@@ -247,7 +247,8 @@
     </el-table>
 
     <!-- 分页 -->
-    <table-page ref="tablePage"></table-page>
+    <table-page ref="tablePage"
+                :page="page"></table-page>
     <!-- 表单 -->
     <dialog-form ref="dialogForm"
                  :columnFormOption="columnFormOption"
@@ -475,12 +476,6 @@ export default create({
       },
       deep: true
     },
-    page: {
-      handler () {
-        this.$refs.tablePage.pageInit();
-      },
-      deep: true
-    },
     propOption () {
       this.$refs.dialogColumn.columnInit();
     },
@@ -638,8 +633,8 @@ export default create({
       return (
         index +
         1 +
-        ((this.$refs.tablePage.defaultPage.currentPage || 1) - 1) *
-        (this.$refs.tablePage.defaultPage.pageSize || 10)
+        ((this.page.currentPage || 1) - 1) *
+        (this.page.pageSize || 10)
       );
     },
     formVal () {
@@ -669,7 +664,7 @@ export default create({
     //刷新事件
     refreshChange () {
       this.$emit("refresh-change", {
-        page: this.$refs.tablePage.defaultPage,
+        page: this.page.defaultPage,
         searchForm: this.$refs.headerSearch.searchForm
       });
     },
--- packages/element-ui/crud/table-page.vue
+++ packages/element-ui/crud/table-page.vue
@@ -2,7 +2,7 @@
   <div :class="b('pagination')"
        v-if="pageFlag">
     <el-pagination :small="crud.isMobile"
-                   :pager-count="crud.tableOption.pagerCount"
+                   :pager-count="defaultPage.pagerCount"
                    :current-page.sync="defaultPage.currentPage"
                    :background="vaildData(defaultPage.pageBackground,config.pageBackground)"
                    :page-size="defaultPage.pageSize"
@@ -17,15 +17,23 @@
 <script>
 import config from "./config";
 import create from "core/create";
-import { vaildData } from "utils/util";
 export default create({
   name: "crud",
   inject: ["crud"],
+  props: {
+    page: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
   data () {
     return {
       config: config,
       defaultPage: {
         total: 0, // 总页数
+        pagerCount: 7,//超过多少条隐藏
         currentPage: 1, // 当前页数
         pageSize: 10, // 每页显示多少条
         pageSizes: [10, 20, 30, 40, 50, 100],
@@ -34,40 +42,41 @@ export default create({
     };
   },
   created () {
-    this.vaildData = vaildData;
-    //初始化分页
     this.pageInit();
     this.crud.$emit("on-load", this.defaultPage);
   },
+  watch: {
+    page: {
+      handler () {
+        this.pageInit();
+      },
+      deep: true,
+    },
+  },
   computed: {
     pageFlag () {
-      return !this.validatenull(this.crud.page.total);
+      return this.defaultPage.total != 0
     }
   },
   methods: {
     pageInit () {
-      this.defaultPage.total = this.crud.page.total || 0;
-      this.defaultPage.currentPage = this.crud.page.currentPage || 1;
-      this.defaultPage.pageSize = this.crud.page.pageSize || 10;
-      this.defaultPage.pageSizes = this.crud.page.pageSizes || [
-        10,
-        20,
-        30,
-        40,
-        50,
-        100
-      ];
-      this.defaultPage.background = this.crud.page.background || true;
+      this.defaultPage = Object.assign(this.defaultPage, this.page)
+      this.updateValue();
+    },
+    updateValue () {
+      this.crud.$emit('update:page', this.defaultPage)
     },
     // 页大小回调
     sizeChange (val) {
       this.defaultPage.currentPage = 1;
       this.defaultPage.pageSize = val;
+      this.updateValue();
       this.crud.$emit("on-load", this.defaultPage);
       this.crud.$emit("size-change", val);
     },
     // 页码回调
     currentChange (val) {
+      this.updateValue();
       this.crud.$emit("on-load", this.defaultPage);
       this.crud.$emit("current-change", val);
     }
