69e46e00665a992969adf2365bd7a0d00d321d11
libchaos <libchaos01@163.com>
2020-04-28 15:17:37 +0800

    Update _export2Excel.js
    删掉日期判断，直接以输入的json的日期格式为准

--- src/plugin/export/_export2Excel.js
+++ src/plugin/export/_export2Excel.js
@@ -73,12 +73,7 @@ function sheet_from_array_of_arrays(data, opts) {
 
       if (typeof cell.v === 'number') cell.t = 'n';
       else if (typeof cell.v === 'boolean') cell.t = 'b';
-      else if (cell.v instanceof Date) {
-        cell.t = 'n';
-        cell.z = XLSX.SSF._table[14];
-        cell.v = datenum(cell.v);
-      } else cell.t = 's';
-
+      else cell.t = 's';
       ws[cell_ref] = cell;
     }
   }
