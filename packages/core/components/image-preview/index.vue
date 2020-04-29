275b6b084352448a176f4d695826bc893e21cd8b
smallwei <1634566606@qq.com>
2020-04-29 15:58:12 +0800

    feat:more img hover gitee_I16LSF

--- packages/core/components/image-preview/index.vue
+++ packages/core/components/image-preview/index.vue
@@ -1,38 +1,35 @@
 <template>
   <div :class="b()"
-       v-show="isShow"
+       v-if="isShow"
        @click="isShow=false">
     <div :class="b('mask')"></div>
-    <div :class="b('box')"
-         :style="previewStyle"
-         v-loading="changeing">
-      <img ref="img"
-           hidden
-           :src="datas[index].url" />
-      <el-image :src="datas[index].url"
-                v-show="!changeing"
-                @load="initStyle"></el-image>
+    <div :class="b('box')">
+      <el-carousel :initial-index="index"
+                   type="card">
+        <el-carousel-item v-for="(item,index) in datas"
+                          :key="index">
+          <el-image :src="item.url"></el-image>
+        </el-carousel-item>
+      </el-carousel>
     </div>
   </div>
-
 </template>
 <script>
 import create from "core/create";
 export default create({
   name: "image-preview",
-  data() {
+  data () {
     return {
       isShow: false,
       datas: [],
       index: 0,
       onClose: null,
-      changeing: false,
       width: 200,
       height: 200
     };
   },
   watch: {
-    isShow() {
+    isShow () {
       if (!this.isShow) {
         this.close();
         this.width = 200;
@@ -40,33 +37,9 @@ export default create({
       }
     }
   },
-  mounted() {},
-  computed: {
-    previewStyle() {
-      return {
-        width: `${this.width}px`,
-        height: `${this.height}px`
-      };
-    }
-  },
+  mounted () { },
   methods: {
-    initStyle(e) {
-      this.changeing = true;
-      let width = this.$refs.img.width;
-      let height = this.$refs.img.height;
-      if (width > 800 || height > 800) {
-        let percent = Math.max(width, height) / 800;
-        width = width / percent;
-        height = height / percent;
-      }
-      this.width = width;
-      this.height = height;
-      setTimeout(() => {
-        this.$refs.img.style.width = "100%";
-        this.changeing = false;
-      }, 300);
-    },
-    close() {
+    close () {
       if (typeof this.onClose === "function") {
         this.onClose(this);
       }
