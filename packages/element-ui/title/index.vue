f8bd050d2ed274ff4516f9145fe39e1884d58dd4
smallwei <1634566606@qq.com>
2020-07-28 14:44:25 +0800

    feat:title block

---
+++ packages/element-ui/title/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="b()">
+    <p :style="styles">{{text}}</p>
+  </div>
+</template>
+
+<script>
+import create from "core/create";
+import props from "../../core/common/props.js";
+import event from "../../core/common/event.js";
+export default create({
+  name: 'title',
+  mixins: [props(), event()],
+  props: {
+    styles: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+})
+</script>
+
