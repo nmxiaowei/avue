b1800cf25fd4f5810cf8bafccb054cfb980a9b4e
smallwei <1634566606@qq.com>
2020-10-22 17:24:27 +0800

    fix:dynamic delete gitee_I1YVPO

--- packages/core/common/event.js
+++ packages/core/common/event.js
@@ -37,14 +37,6 @@ export default function () {
       },
       handleChange (value) {
         let result = value;
-        if (this.$AVUE.ui.name == 'antd') {
-          if (['date', 'time'].includes(this.type)) {
-            const format = this.format.replace('dd', 'DD').replace('yyyy', 'YYYY');
-            result = dayjs(value._d).format(format);
-          } else if (['radio'].includes(this.type)) {
-            result = value.target.value;
-          }
-        }
         if (this.isString || this.isNumber || this.stringMode || this.listType === "picture-img") {
           if (Array.isArray(value)) result = value.join(',')
         }
