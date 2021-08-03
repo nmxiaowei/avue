485f2a97c24d331f95ee83082ec07382a5c0dba1
smallwei <1634566606@qq.com>
2021-08-03 11:46:15 +0800

    styles:checkbox all

--- packages/core/common/event.js
+++ packages/core/common/event.js
@@ -1,6 +1,5 @@
 import { initVal } from 'core/dataformat';
 import { DIC_SPLIT } from 'global/variable';
-import { strCorNum } from 'utils/util';
 function bindEvent (safe, name, event) {
   typeof safe[name] === 'function' && safe[name]({ value: safe.value, column: safe.column })
   safe.$emit(name, safe.value, event)
@@ -33,7 +32,6 @@ export default function () {
         let flag = this.isString || this.isNumber || this.stringMode;
         if (flag && Array.isArray(value)) {
           result = value.join(this.separator || DIC_SPLIT)
-          if (this.isNumber) result = strCorNum(result)
         }
         if (typeof this.change === 'function' && this.column.cell !== true) {
           this.change({ value: result, column: this.column });
