00d3b13382946080462b46f2b6d77a634cdde420
smallwei <1634566606@qq.com>
2021-10-08 08:41:02 +0800

    feat:crud html params gitee_I4CK8U

--- packages/element-ui/crud/column-slot.vue
+++ packages/element-ui/crud/column-slot.vue
@@ -100,8 +100,10 @@
           <avue-rate disabled
                      v-model="row[column.prop]" />
         </span>
-        <span v-else
+        <span v-else-if="column.html"
               v-html="handleDetail(row,column)"></span>
+        <span v-else
+              v-text="handleDetail(row,column)"></span>
       </template>
     </template>
   </el-table-column>
