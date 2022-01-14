f55f6fcd49bfede47bdac58c74d4fad3e234aaee
smallwei <1634566606@qq.com>
2022-01-14 15:38:18 +0800

    fix:validatenull util

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,19 +1,11 @@
-import { deepClone } from './util';
 /**
  * 判断是否为空
  */
 export function validatenull (val) {
-  // 特殊判断
-  if (val && parseInt(val) === 0) return false;
-  const list = ['$parent'];
   if (val instanceof Date || typeof val === 'boolean' || typeof val === 'number') return false;
   if (val instanceof Array) {
     if (val.length === 0) return true;
   } else if (val instanceof Object) {
-    val = deepClone(val);
-    list.forEach(ele => {
-      delete val[ele];
-    });
     for (var o in val) {
       return false;
     }
