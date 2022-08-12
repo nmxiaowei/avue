c2104f3e0929352b873bf116c3b9c3881cd27b4c
smallwei <1634566606@qq.com>
2022-08-12 10:39:57 +0800

    feat:code block

--- packages/element-ui/code/index.vue
+++ packages/element-ui/code/index.vue
@@ -1,45 +1,70 @@
 <template>
-  <div :class="b()">
-    <el-scrollbar :style="styleName">
-      <pre>
-        <code :class="syntax" ref="container">
-          <slot></slot>
-        </code>
-      </pre>
-    </el-scrollbar>
+  <div :class="[b(),className]"
+       :style="styleSizeName"
+       ref="main">
+    <component :is="id"
+               v-if="reload">
+    </component>
   </div>
-
 </template>
 
 <script>
+import Vue from 'vue'
+import { uuid } from 'utils/util'
 import create from "core/create";
-import packages from "core/packages";
 export default create({
   name: "code",
   props: {
-    height: {
-      type: Number,
-      default: 200
+    code: String
+  },
+  data () {
+    return {
+      id: 'code_' + uuid(),
+      reload: false
+    };
+  },
+  watch: {
+    code () {
+      this.initVue()
+    }
+  },
+  mounted () {
+    this.initVue()
+  },
+  methods: {
+    initVue () {
+      this.reload = false
+      let template = this.getSource("template");
+      if (!template) return
+      let script = this.getSource("script");
+      if (script) {
+        script = script.replace(/export default/, "return");
+      }
+      let styleCss = this.getSource("style");
+      let styleId = 'style-' + this.id;
+      if (document.getElementById(styleId)) {
+        document.getElementById(styleId).remove()
+      }
+      let style = document.createElement("style");
+      style.id = styleId
+      style.innerHTML = styleCss;
+      document.head.appendChild(style);
+      let obj = new Function(script)();
+      obj.template = template;
+      Vue.component(this.id, obj)
+      this.$nextTick(() => {
+        this.reload = true
+      })
     },
-    syntax: {
-      type: String,
-      default: "javascript"
-    }
-  },
-  computed: {
-    styleName() {
-      return {
-        height: this.setPx(this.height)
-      };
-    }
-  },
-  mounted() {
-    if (!window.hljs) {
-      packages.logs("hljs");
-      return;
-    }
-    if (window.hljs && typeof window.hljs.highlightBlock === "function") {
-      window.hljs.highlightBlock(this.$refs["container"]);
+    getSource (type) {
+      const reg = new RegExp(`<${type}[^>]*>`);
+      let content = this.code;
+      let matches = content.match(reg);
+      if (matches) {
+        let start = content.indexOf(matches[0]) + matches[0].length;
+        let end = content.lastIndexOf(`</${type}`);
+        return content.slice(start, end)
+      }
     }
   }
 });
