392a6b582bbdb8b988fd6d46964df1f55f0eeb07
smallwei <1634566606@qq.com>
2022-12-08 19:57:55 +0800

    fix:dialog excel option select prop

--- packages/element-ui/crud/dialog-excel.vue
+++ packages/element-ui/crud/dialog-excel.vue
@@ -7,8 +7,10 @@
                class="avue-dialog"
                :visible.sync="box"
                :width="crud.isMobile?'100%':'30%'">
-      <avue-form v-model="form"
-                 :option="option"></avue-form>
+      <avue-form ref="form"
+                 v-model="form"
+                 :option="option">
+      </avue-form>
       <span slot="footer"
             class="dialog-footer">
         <el-button type="primary"
@@ -144,28 +146,34 @@ export default {
       this.form.type = this.crud.selectLen == 0
     },
     getColumnOption () {
-      let result = []
       let column = this.deepClone(this.crud.columnOption)
-      column.forEach(ele => {
-        let children = ele.children
-        if (children && !Array.isArray(children)) {
-          delete ele.children
-        }
-        if (ele.showColumn !== false) result.push(ele)
-      })
-      this.columnOption = result;
-      this.form.prop = this.columnOption.map(ele => ele.prop)
+      let prop = []
+      const findProp = (list = []) => {
+        list.forEach((ele, index) => {
+          let children = ele.children
+          if (children && !Array.isArray(children)) delete ele.children
+          else if (ele.showColumn === false) list.splice(index, 1)
+          else {
+            prop.push(ele.prop)
+            if (ele.children) findProp(children)
+          }
+        })
+      }
+      findProp(column)
+      this.columnOption = column;
+      this.form.prop = prop
     },
     getColumn () {
       let columns = this.deepClone(this.columnOption);
+      let props = this.$refs.form.getPropRef('prop').$refs.temp.getHalfList()
       if (!this.form.params) return []
       if (this.form.params.includes('headers')) {
         const findProp = (list = []) => {
           list.forEach((ele, index) => {
-            if (ele.children) {
-              findProp(ele.children)
-            } else if (!this.form.prop.includes(ele.prop)) {
+            if (!props.includes(ele.prop)) {
               list.splice(index, 1);
+            } else if (ele.children) {
+              findProp(ele.children)
             }
           })
         }
@@ -177,7 +185,7 @@ export default {
           list.forEach((ele, index) => {
             if (ele.children) {
               findProp(ele.children)
-            } else if (this.form.prop.includes(ele.prop)) {
+            } else if (props.includes(ele.prop)) {
               result.push(ele)
             }
           })
