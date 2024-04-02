50a6f9f74adc44b0880887d2dcef02b0693e51c2
smallwei <1634566606@qq.com>
2024-04-02 14:07:46 +0800

    feat:crud grid suprt selection

--- packages/element-ui/crud/grid/index.vue
+++ packages/element-ui/crud/grid/index.vue
@@ -4,38 +4,47 @@
     <div :class="b('hide')">
       <slot></slot>
     </div>
-    <el-row v-if="data.length!==0">
-      <el-col v-for="(c,p) in data"
-              @click.stop="handleRowClick(c,column)"
-              @dblclick.stop="handleRowDblClick(c,column)"
-              :span="crud.tableOption.gridSpan || span"
-              :md="crud.tableOption.gridSpan|| span"
-              :sm="crud.tableOption.gridSpan|| span"
-              :xs="crud.tableOption.gridXsSpan|| xsSpan"
-              :class="getRowClass(c,column)"
-              :key="p">
-        <div :class="b('content')"
-             :style="getGradientColor()">
-          <div v-for="(item,index) in column"
-               :class="[b('item'),(item.type || item.prop),getClass(c,item)]"
-               @click="handleCellClick(c,item)"
-               @dblclick="handleCellDblClick(c,item)"
-               :key="index">
-            <template v-for="(comp,cindex) in item.header && item.header({row:c,$index:p})"
-                      :key="cindex">
-              <component :class="[b('label'),item.labelClassName]"
-                         :is="comp"></component>
-            </template>
-            <template v-for="(comp,cindex) in item.default &&item.default({row:c,$index:p})"
-                      :key="cindex">
-              <div :class="[b('value'),item.className]">
-                <component :is="comp"></component>
-              </div>
-            </template>
+    <el-checkbox-group v-if="data.length!==0"
+                       v-model="checkList"
+                       @change="checkListChange">
+      <el-row>
+        <el-col v-for="(c,p) in data"
+                @click.stop="handleRowClick(c,column)"
+                @dblclick.stop="handleRowDblClick(c,column)"
+                :span="crud.tableOption.gridSpan || span"
+                :md="crud.tableOption.gridSpan|| span"
+                :sm="crud.tableOption.gridSpan|| span"
+                :xs="crud.tableOption.gridXsSpan|| xsSpan"
+                :class="getRowClass(c,column)"
+                :key="p">
+          <div :class="b('content')"
+               :style="getGradientColor()">
+            <div v-for="(item,index) in column"
+                 :class="[b('item'),(item.type || item.prop),getClass(c,item)]"
+                 @click="handleCellClick(c,item)"
+                 @dblclick="handleCellDblClick(c,item)"
+                 :key="index">
+              <span v-if="item.type=='selection'">
+                <el-checkbox :label="p">&nbsp;</el-checkbox>
+              </span>
+              <template v-else>
+                <template v-for="(comp,cindex) in item.header && item.header({row:c,$index:p})"
+                          :key="cindex">
+                  <component :class="[b('label'),item.labelClassName]"
+                             :is="comp"></component>
+                </template>
+                <template v-for="(comp,cindex) in item.default &&item.default({row:c,$index:p})"
+                          :key="cindex">
+                  <div :class="[b('value'),item.className]">
+                    <component :is="comp"></component>
+                  </div>
+                </template>
+              </template>
+            </div>
           </div>
-        </div>
-      </el-col>
-    </el-row>
+        </el-col>
+      </el-row>
+    </el-checkbox-group>
     <el-empty v-else
               :image-size="100"
               :description="crud.tableOption.emptyText || t('crud.emptyText')"></el-empty>
@@ -57,6 +66,7 @@ export default create({
   },
   data () {
     return {
+      checkList: [],
       span: 8,
       xsSpan: 12,
       id: 'crud-grid',
@@ -71,6 +81,36 @@ export default create({
     }
   },
   methods: {
+    clearSelection () {
+      this.checkList = []
+      this.checkListChange(this.checkList)
+    },
+    toggleAllSelection () {
+      if (this.checkList.length === this.crud.data.length) {
+        this.checkList = []
+      } else {
+        this.checkList = this.crud.data.map((ele, index) => index)
+      }
+      this.checkListChange(this.checkList)
+    },
+    toggleRowSelection (data, selected) {
+      let index = this.crud.data.findIndex(ele => JSON.stringify(ele) == JSON.stringify(data))
+      if (selected && index != -1) {
+        this.checkList.push(index)
+      } else {
+        let checkIndex = this.checkList.findIndex(ele => ele == index);
+        this.checkList.splice(checkIndex, 1)
+      }
+      this.checkListChange(this.checkList)
+    },
+    checkListChange (val) {
+      let result = [];
+      const data = this.crud.data
+      val.forEach(ele => {
+        result.push(data[ele])
+      });
+      this.$emit('selection-change', result)
+    },
     handleRowDblClick (row, index) {
       this.$emit('row-dblclick', row, index)
     },
