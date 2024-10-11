d7908625b04edf55459be33449612f62922d5198
smallwei <1634566606@qq.com>
2024-10-11 20:04:42 +0800

    fix:crud dateBtn shortcuts gitee_IAW8M3

--- packages/element-ui/crud/menu/header-menu.vue
+++ packages/element-ui/crud/menu/header-menu.vue
@@ -112,47 +112,28 @@ export default create({
   data () {
     return {
       dateCreate: false,
-      shortcuts: [{
-        text: '今日',
-        onClick (picker) {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime());
-          picker.$emit('pick', [start, end]);
-        }
-      }, {
-        text: '昨日',
-        onClick (picker) {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime() - 3600 * 1000 * 24 * 1);
-          picker.$emit('pick', [start, end]);
-        }
-      }, {
-        text: '最近一周',
-        onClick (picker) {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-          picker.$emit('pick', [start, end]);
-        }
-      }, {
-        text: '最近一个月',
-        onClick (picker) {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-          picker.$emit('pick', [start, end]);
-        }
-      }, {
-        text: '最近三个月',
-        onClick (picker) {
-          const end = new Date();
-          const start = new Date();
-          start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-          picker.$emit('pick', [start, end]);
-        }
-      }],
+      shortcuts: [
+        {
+          text: '今天',
+          value: new Date(),
+        },
+        {
+          text: '昨天',
+          value: () => {
+            const date = new Date();
+            date.setTime(date.getTime() - 3600 * 1000 * 24);
+            return date;
+          },
+        },
+        {
+          text: '一周前',
+          value: () => {
+            const date = new Date();
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+            return date;
+          },
+        },
+      ],,
       config: config
     };
   },
