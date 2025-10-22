4ea667c8ebf03ec0e4806144a47816af4d473555
smallwei <1634566606@qq.com>
2025-10-22 11:57:04 +0800

    fix:table value default gitee_ID2RQ4

--- packages/element-plus/input-table/index.vue
+++ packages/element-plus/input-table/index.vue
@@ -180,16 +180,13 @@ export default create({
       if (typeof this.onLoad == "function") {
         this.onLoad({ value: this.text }, (data) => {
           let result = Array.isArray(data) ? data : [data];
-          const nV = this.modelValue;
-          const activeObjArray =
-            nV && nV.length
-              ? result.filter((item) =>
-                  nV.some((val) => val === item[this.valueKey])
-                )
-              : [];
+          const value = this.modelValue;
+          const valueArray = result.filter((item) =>
+            value.some((val) => val == item[this.valueKey])
+          );
 
-          this.object = this.deepClone(activeObjArray)
-          this.active = this.deepClone(activeObjArray)
+          this.object = this.deepClone(result);
+          this.active = this.deepClone(result);
 
           this.created = true;
         });
